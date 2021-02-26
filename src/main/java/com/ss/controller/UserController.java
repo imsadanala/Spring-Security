3138a3feb5d5bd7184fb7f30ba1c6cfdca2c2c46
Sadanala <ssadanala@deloitte.com>
2021-02-26 09:00:44 +0530

    fox swagger2 changes

--- src/main/java/com/ss/controller/UserController.java
+++ src/main/java/com/ss/controller/UserController.java
@@ -5,19 +5,14 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import com.ss.dto.User;
-import com.ss.exception.UserNotFoundExeption;
 import com.ss.service.UserService;
 
 @RestController
@@ -31,18 +26,18 @@ public class UserController {
 		return userService.findAll();
 	}
 
-	@GetMapping(path = "/users/{id}")
-	public EntityModel<User> getUser(@PathVariable Integer id) {
-
-		User user = userService.findById(id);
-		if (user == null) {
-			throw new UserNotFoundExeption("id-" + id);
-		}
-		EntityModel<User> resource = EntityModel.of(user);
-		WebMvcLinkBuilder retrieveUsersLink = linkTo(methodOn(this.getClass()).getUsers());
-		resource.add(retrieveUsersLink.withRel("all-users"));
-		return resource;
-	}
+//	@GetMapping(path = "/users/{id}")
+//	public EntityModel<User> getUser(@PathVariable Integer id) {
+//
+//		User user = userService.findById(id);
+//		if (user == null) {
+//			throw new UserNotFoundExeption("id-" + id);
+//		}
+//		EntityModel<User> resource = EntityModel.of(user);
+//		WebMvcLinkBuilder retrieveUsersLink = linkTo(methodOn(this.getClass()).getUsers());
+//		resource.add(retrieveUsersLink.withRel("all-users"));
+//		return resource;
+//	}
 
 	@PostMapping(path = "/users")
 	public User add(@Valid @RequestBody User user) {
