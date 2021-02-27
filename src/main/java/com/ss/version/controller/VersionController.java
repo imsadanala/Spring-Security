5913d68d1e42d4e6dafd2d576c03a757ceaa7645
Sadanala <ssadanala@deloitte.com>
2021-02-27 07:28:57 +0530

    Versioning using headers

--- src/main/java/com/ss/version/controller/VersionController.java
+++ src/main/java/com/ss/version/controller/VersionController.java
@@ -20,13 +20,43 @@ public class VersionController {
 		return new PersonV2(new Name("Suresh", "Sadanala"));
 	}
 
+	/**
+	 * Add API-VERSION attritube in the headers section
+	 * 
+	 * @return
+	 */
 	@GetMapping(value = "/person", headers = "API-VERSION=1")
 	public PersonV1 personH1() {
 		return new PersonV1("Suresh Sadanala");
 	}
 
+	/**
+	 * Add API-VERSION attritube in the headers section
+	 * 
+	 * @return
+	 */
 	@GetMapping(value = "/person", headers = "API-VERSION=2")
 	public PersonV2 personH2() {
+		return new PersonV2(new Name("Suresh", "Sadanala"));
+	}
+
+	/**
+	 * Add the application/v1+json in the ACCEPT in the request
+	 * 
+	 * @return
+	 */
+	@GetMapping(value = "person", produces = "application/v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Suresh Sadanala");
+	}
+
+	/**
+	 * Add the application/v2+json in the ACCEPT in the request
+	 * 
+	 * @return
+	 */
+	@GetMapping(value = "person", produces = "application/v2+json")
+	public PersonV2 producesV2() {
 		return new PersonV2(new Name("Suresh", "Sadanala"));
 	}
 }
